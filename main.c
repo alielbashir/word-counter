5cd57b448b464127bea77507c46d4a7520865e84
akiinerdem <akiinerdem@gmail.com>
2020-12-16 15:24:38 +0300

    add list

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
+#include <conio.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct List {
+    char *word;
+    struct List *next;
+};
+typedef struct List Node;
+Node *head;
+
+void append(char *word) {
+    head->word = word;
+    Node *tmp = (Node *) malloc(sizeof(Node));
+    tmp->next = NULL;
+    head->next = tmp;
+    printf("Added : %s\n", head->word);
+    head = head->next;
+}
+
+void readToList(char *line) {
+    char *word = NULL;
+    word = strtok(line, " ");
+    while (word != NULL) {
+        append(word);
+        word = strtok(NULL, " ");
+    }
+}
 
 int main() {
-    printf("Word Counter\n");
+
+    head = (Node *) malloc(sizeof(Node));
+    head->next = NULL;
+    head = head;
+    FILE *fp = fopen("input.txt", "r");
+    char line[1000];
+    while (fgets(line, sizeof(line), fp))
+        readToList(line);
+    fclose(fp);
+    getch();
+
     return 0;
 }
