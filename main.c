73f8703afd7575459f71e37d03077d7106867e6a
alielbashiir@gmail.com <qtGGp8STBnWxt4V>
2020-12-18 00:00:03 +0300

    Add functioning add to end function
    The program now functions as a doubly-linked list, with all words
    added to the end. Future commits will handle cases for which the word
    already exists, and only increment its value

--- main.c
+++ main.c
@@ -12,78 +12,83 @@ struct List {
 
 };
 typedef struct List Node;
-
+Node *head; // first node
+Node *tail; // last node
 
 Node *getNode() {
     Node *node = (Node *) malloc(sizeof(Node));
+    node->word = NULL;
     node->count = 0;
     node->next = NULL;
     node->prev = NULL;
     return node;
 }
 
-void addToMiddle(char *word, Node *node) {
+//void addToMiddle(char *word, Node *node) {
+//
+//    // create node and populate it
+//    Node *newNode = getNode();
+//    newNode->word = word;
+//    newNode->count++;
+//
+//}
 
-    // create node and populate it
-    Node *newNode = getNode();
-    newNode->word = word;
-    newNode->count++;
 
+
+void addToEnd(const char *word) {
+    // handling for 1st node
+    if (head->word == NULL) {
+        // allocate space for word and copy it
+        head->word = malloc(strlen(word) + 1);
+        strcpy(head->word, word);
+        // increment count and add tail pointer
+        head->count++;
+        head->next = tail;
+    }
+    // create and populate new node
+    Node *node = getNode();
+    node->word = malloc(strlen(word) + 1);
+    strcpy(node->word, word);
+    // increment count
+    node->count ++;
+    // add next and prev pointers, and set new node as tail
+    node->prev = tail;
+    tail->next = node;
+    tail = node;
+    printf("Added : %s\n", tail->word);
 }
 
-_Noreturn void addToBeginning(char *word, Node *node) {
-    // word: kelime
-    // node: listede herhangi bir node
-
-    // ilk elemani arama islemi
-    while (1) {
-        if (node->prev == NULL) {
-            // yeni nodeu tanimla ve doldur
-            Node *newNode = getNode();
-            newNode->word = word;
-            newNode->count++;
-            newNode->next = node;
-            // yeni node, ilk nodeun sonuna ekle
-            node->prev = newNode;
-        }
-        node = node->prev;
+void add(const char* word) {
+    // add as last element
+    addToEnd(word);
+}
+void printList() {
+    Node* node = head;
+    while (node != NULL) {
+        printf("%s : %d\n", node->word, node->count);
+        node = node->next;
     }
 }
 
-void append(char *word, Node *node) {
-    node->word = word;
-    Node *tmp = getNode();
-    tmp->prev = node;
-    node->next = tmp;
-    printf("Added : %s\n", node->word);
-    // FIXME: orphaned
-    node = node->next;
-}
-
-
-void printList(Node *root) {
-
-}
-
-void readToList(char *line, Node *node) {
+void readToList(char *line) {
     char *word = NULL;
     word = strtok(line, " ");
-    printf("\nı\n");
     while (word != NULL) {
-        append(word, node);
+        add(word);
         word = strtok(NULL, " ");
     }
 }
 
 
 int main() {
-    Node *head = getNode();
-    Node *iter = head;
+    head = getNode();
+    tail = head;
     FILE *fp = fopen("input.txt", "r");
     char line[1000];
     while (fgets(line, sizeof(line), fp))
-        readToList(line, iter);
+        readToList(line);
     fclose(fp);
+    printList();
 
     return 0;
 }
