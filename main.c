5a7f504f1d0846c2efb64d6e36de81d11401328d
alielbashiir@gmail.com <qtGGp8STBnWxt4V>
2020-12-18 03:44:43 +0300

    Implement real-time sorting
    The program now sorts the words as they are read

--- main.c
+++ main.c
@@ -4,9 +4,9 @@
 #include <stdbool.h>
 
 // TODO:
-//  1. add real-time sorting
-//  2. add add to middle function
-//  3. free all resources
+//  1. remove tail. More cons than pros
+//  2. free all resources
+//  3. fix newline parsing
 struct List {
     char *word;
     int count;
@@ -26,14 +26,47 @@ Node *getNode() {
     return node;
 }
 
-void addToMiddle(const char *word, Node *node) {
+void extract(Node *node) {
+    // removes node from current position, connecting its previous and next elements
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    node->next = NULL;
+    node->prev = NULL;
+}
 
-    // create node and populate it
-    Node *newNode = getNode();
-    newNode->word = word;
-    newNode->count++;
+void insert(Node *node1, Node *node2) {
+    // node1: node to be inserted behind node2
+    // node2: node to be in front of node1
+    node2->prev->next = node1;
+    node1->prev = node2->prev;
+
+    node1->next = node2;
+    node2->prev = node1;
+}
+
+void addToMiddle(Node *node) {
+
+    // increment word count
+    node->count++;
+    Node *tmp = node->prev;
+    // if the node is the head then don't do anything. It is a 2 element list
+    if (node == head) {
+        return;
+    }
+    // iterate if tmp's count is lower than node's
+    while (tmp->count < node->count) {
+        // if tmp's previous node's count is larger than or equal to node's count
+        if (tmp->prev->count >= node->count) {
+            extract(node); // remove node from where it was, while connecting it's previous and next elements
+            insert(node, tmp); // insert node behind tmp
+            break;
+        }
+        tmp = tmp->prev;
+    }
+    printf("incremented : %s\n", node->word);
 
 }
+
 
 void addToBeginning(const char *word) {
 
@@ -61,31 +94,36 @@ void addToEnd(const char *word) {
     printf("Added : %s\n", tail->word);
 }
 
-void add(const char *word) {
-    // TODO: clean up logic
-
-    // handling for 1st node
+bool searchAndAdd(const char *word) {
     Node *node = head;
-    if (node->word == NULL) {
-        addToBeginning(word);
-        return;
-    }
+    bool b = false;
     // linear search for word
     while (node != NULL) {
+        // if found
         if (strcmp(node->word, word) == 0) {
-            // increment word count
-            node->count++;
-            printf("incremented : %s\n", node->word);
-//         addToMiddle(word, node);
-         return;
+            addToMiddle(node);
+            return b = true;
         }
         if (node == node->next) {
             break;
         }
         node = node->next;
     }
-    // add as last element
-    addToEnd(word);
+    return b;
+}
+
+void add(const char *word) {
+    // TODO: clean up logic
+
+    // handling for 1st node
+    if (head->word == NULL) {
+        addToBeginning(word);
+    } else if (searchAndAdd(word)) {
+        // do nothing, function side effects handle that
+    } else {
+        // add as last element
+        addToEnd(word);
+    }
 }
 
 void printList() {
